4506f3091e61ab8cb7c161ac3f524106f7b82fb5
avsandiryakov <avsandiryakov@gmail.com>
2021-02-12 17:23:28 +0300

    add lesson_6

---
+++ lesson_6/task_4.py
@@ -0,0 +1,72 @@
+"""
+4. Реализуйте базовый класс Car. У данного класса должны быть следующие атрибуты: speed, color, name, is_police
+(булево). А также методы: go, stop, turn(direction), которые должны сообщать, что машина поехала, остановилась,
+повернула (куда). Опишите несколько дочерних классов: TownCar, SportCar, WorkCar, PoliceCar.
+Добавьте в базовый класс метод show_speed, который должен показывать текущую скорость автомобиля.
+Для классов TownCar и WorkCar переопределите метод show_speed.
+При значении скорости свыше 60 (TownCar) и 40 (WorkCar) должно выводиться сообщение о превышении скорости.
+Создайте экземпляры классов, передайте значения атрибутов. Выполните доступ к атрибутам, выведите результат.
+Выполните вызов методов и также покажите результат.
+"""
+
+
+class Car:
+
+    def __init__(self, speed, color, name, is_police=False):
+        self.speed = speed
+        self.color = color
+        self.name = name
+        self.is_police = is_police
+
+    def go(self):
+        print('Машина поехала')
+
+    def stop(self):
+        print('Машина остановилась')
+
+    def turn(self, direction):
+        print(f'Машина повернула {direction}')
+
+    def show_speed(self):
+        print(self.speed)
+
+
+class TownCar(Car):
+    def __init__(self, speed, color, name, is_police=False):
+        super().__init__(speed, color, name, is_police)
+
+    def show_speed(self):
+        if self.speed > 60:
+            print('Превышение скорости')
+        else:
+            print(self.speed)
+
+
+class SportCar(Car):
+    def __init__(self, speed, color, name, is_police=False):
+        super().__init__(speed, color, name, is_police)
+
+
+class WorkCar(Car):
+    def __init__(self, speed, color, name, is_police=False):
+        super().__init__(speed, color, name, is_police)
+
+    def show_speed(self):
+        if self.speed > 40:
+            print('Превышение скорости')
+        else:
+            print(self.speed)
+
+
+class PoliceCar(Car):
+    def __init__(self, speed, color, name, is_police=True):
+        super().__init__(speed, color, name, is_police)
+
+
+work = WorkCar(speed=20, color='green', name='Mutsubishi')
+work.go()
+work.speed = 40
+work.show_speed()
+work.speed = 100
+work.show_speed()
+work.stop()
